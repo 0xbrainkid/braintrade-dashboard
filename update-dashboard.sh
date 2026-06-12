9b1416e4a0d7379489d4284eb6ff37ba0160f64e
Ubuntu <ubuntu@ip-172-31-14-6.eu-north-1.compute.internal>
2026-06-12 10:00:06 +0000

    update 2026-06-12T10:00

--- update-dashboard.sh
+++ update-dashboard.sh
@@ -1135,6 +1135,7 @@ if _short_aligned_replay:
     _sar_fresh_two_day = _short_aligned_replay.get('fresh_two_day_candidate_audit') or _sar_dedupe.get('fresh_two_day_candidate_audit') or {}
     _sar_freshness = _short_aligned_replay.get('setup_freshness_audit') or _sar_dedupe.get('setup_freshness_audit') or {}
     _sar_stagnation = _short_aligned_replay.get('candidate_stagnation_audit') or _sar_dedupe.get('candidate_stagnation_audit') or {}
+    _sar_setup_trend = _short_aligned_replay.get('setup_funnel_trend_audit') or {}
     _sar_promo = _short_aligned_replay.get('promotion_audit') or {}
     _sar_deduped30 = _sar_promo.get('deduped_30m') or ((_sar_dedupe.get('deduped_horizons') or {}).get('30m') or {})
     _sar_current30 = _sar_promo.get('current_30m') or _sar30
@@ -1147,6 +1148,7 @@ if _short_aligned_replay:
         "latest_candidate_ts": _short_aligned_replay.get('latest_candidate_ts'),
         "setup_freshness_audit": _sar_freshness,
         "candidate_stagnation_audit": _sar_stagnation,
+        "setup_funnel_trend_audit": _sar_setup_trend,
         "setup_freshness_label": _sar_freshness.get('label'),
         "unique_candidate_rows": _sar_dedupe.get('unique_candidate_rows'),
         "latest_unique_candidate_ts": _sar_dedupe.get('latest_unique_candidate_ts'),
@@ -1173,6 +1175,7 @@ if _short_aligned_replay:
     _sar_fresh_two_day = _short_aligned_replay.get('fresh_two_day_candidate_audit') or _sar_dedupe.get('fresh_two_day_candidate_audit') or {}
     _sar_freshness = _short_aligned_replay.get('setup_freshness_audit') or _sar_dedupe.get('setup_freshness_audit') or {}
     _sar_stagnation = _short_aligned_replay.get('candidate_stagnation_audit') or _sar_dedupe.get('candidate_stagnation_audit') or {}
+    _sar_setup_trend = _short_aligned_replay.get('setup_funnel_trend_audit') or {}
     _sar_promo = _short_aligned_replay.get('promotion_audit') or {}
     _sar_current30 = _sar_promo.get('current_30m') or (((_short_aligned_replay.get('horizons') or {}).get('30m')) or {})
     _sar_deduped30 = _sar_promo.get('deduped_30m') or ((_sar_dedupe.get('deduped_horizons') or {}).get('30m') or {})
@@ -1186,6 +1189,7 @@ if _short_aligned_replay:
         "latest_unique_candidate_ts": _sar_dedupe.get('latest_unique_candidate_ts'),
         "setup_freshness_audit": _sar_freshness,
         "candidate_stagnation_audit": _sar_stagnation,
+        "setup_funnel_trend_audit": _sar_setup_trend,
         "setup_freshness_label": _sar_freshness.get('label'),
         "current_30m": _sar_current30,
         "deduped_30m": _sar_deduped30,
